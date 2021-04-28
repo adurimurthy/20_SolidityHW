7a8fee599c3d05cc1d453878b430605bd4aa489b
Murthy Aduri <5318312+adurimurthy@users.noreply.github.com>
2021-04-27 22:22:48 -0400

    Update AssociateProfitSplitter.sol
    with employee addresses

--- AssociateProfitSplitter.sol
+++ AssociateProfitSplitter.sol
@@ -10,9 +10,9 @@ contract AssociateProfitSplitter {
 
     //Setting the variables above as parameters in the constructor below
     constructor(address payable _one, address payable _two, address payable _three) public {
-        employee_one = _one;
-        employee_two = _two;
-        employee_three = _three;
+        employee_one = 0x99A4ee3581F68593ff28E4AD70d2118E6E7a9BE0;
+        employee_two = 0xb2676C7b0d84B9DC8F67fFd6601e0855723d856D;
+        employee_three = 0xA520A62e6B62FB9636D308bc0c0d5f3C05FDEebD;
     }
     //this should return 0 , if not deposit function needs correction
     function balance() public view returns(uint) {
